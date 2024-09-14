fe33fb1b8183bb3cb61c67497db884ec99781ba4
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2024-09-13 23:00:43 -0400

    componente de cursos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,12 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap(),tailwind({
+        applyBaseStyles: false,
+      }),, react()],
 });
