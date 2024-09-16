c5351d5c1c1bb6d2df601735b3206a97e77b8f3f
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2024-09-16 19:49:14 -0400

    config dns

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # investigacion-digital
+
+Cursos de herramientas digitales para la investigación social.
+https://investigacion-digital.org/
--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(),tailwind({
-        applyBaseStyles: false,
-      }),, react()],
+  site: 'https://investigacion-digital.org',
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false,
+  }), , react()],
 });
