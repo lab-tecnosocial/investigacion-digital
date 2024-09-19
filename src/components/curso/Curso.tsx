ee5c2636818297dde502a6f2909548a602d377a7
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2024-09-19 19:38:00 -0400

    estilo de curso mejorado

--- src/components/curso/Curso.tsx
+++ src/components/curso/Curso.tsx
@@ -3,78 +3,80 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
-import { ArrowLeft, Calendar, Clock, Users } from 'lucide-react'
+import { ArrowLeft, Calendar, Clock, Users, LibraryBig } from 'lucide-react'
+
 import type { Curso as TCurso } from '@/data/cursosTypes';
 
 export default function Curso({ curso }: { curso: TCurso }) {
 
   return (
-    <main className="container mx-auto py-8 max-w-4xl">
-      <Button variant="ghost" className="mb-4" asChild >
-        <a href="/catalogo">
-          <ArrowLeft className="mr-2 h-4 w-4" /> Vuelve al catalogo
-        </a>
-      </Button>
-
-      <Card className="mb-8">
-        <CardHeader>
-          <img src={'/images/' + curso.imagen} alt={curso.titulo} className="w-full h-64 object-cover rounded-t-lg" />
-          <CardTitle className="text-3xl mt-4">{curso.titulo}</CardTitle>
-          <CardDescription><b>Competencia:</b> {curso.competencia}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-wrap gap-4 mb-4">
-            <Badge variant="secondary" className="flex items-center">
-              <Clock className="mr-1 h-4 w-4" /> { }
-            </Badge>
-            <Badge variant="secondary" className="flex items-center">
-              <Calendar className="mr-1 h-4 w-4" /> { }
-            </Badge>
-            <Badge variant="secondary" className="flex items-center">
-              <Users className="mr-1 h-4 w-4" /> { } inscritos
-            </Badge>
-          </div>
-
-          <Tabs defaultValue="curriculum">
-            <TabsList>
-              <TabsTrigger value="curriculum">Contenido</TabsTrigger>
-              <TabsTrigger value="instructor">Docente</TabsTrigger>
-            </TabsList>
-            <TabsContent value="curriculum">
-              <ol className="list-decimal pl-5 space-y-2">
-                {curso.contenidos.map((item, index) => (
-                  <>
-                    <li key={index}>{item.titulo}</li>
-                    {item.subtitulos && (
-                      <ul className="list-disc pl-5 space-y-2">
-                        {item.subtitulos.map((subitem, subindex) => (
-                          <li key={subindex}>{subitem}</li>
-                        ))}
-                      </ul>
-                    )}
-                  </>
-                ))}
-              </ol>
-            </TabsContent>
-            <TabsContent value="instructor">
-              <div className="flex items-center space-x-4 mb-4">
-                <Avatar>
-                  <AvatarImage src={'/images/' + curso.docente.foto} alt={curso.docente.nombre} />
-                </Avatar>
-                <div>
-                  <h3 className="font-semibold">{curso.docente.nombre}</h3>
-                  <p className="text-sm text-gray-500">{curso.docente.bio}</p>
+    <main>
+      <img src={'/images/' + curso.imagen} alt={curso.titulo} className="w-full h-72 object-cover" />
+      <div className="container mx-auto py-8 max-w-4xl">
+        <h1 className="text-3xl font-title text-center px-4">{curso.titulo}</h1>
+        <h2 className="font-body my-2 px-4"><b>Competencia:</b> {curso.competencia}</h2>
+        <Card className="font-body">
+          <CardContent>
+            <div className="flex flex-wrap gap-4 my-4 justify-center">
+              <Badge variant="secondary" className="flex items-center">
+                <Clock className="mr-1 h-4 w-4" /> { }
+              </Badge>
+              <Badge variant="secondary" className="flex items-center">
+                <Calendar className="mr-1 h-4 w-4" /> { }
+              </Badge>
+              <Badge variant="secondary" className="flex items-center">
+                <Users className="mr-1 h-4 w-4" /> { } inscritos
+              </Badge>
+            </div>
+            <Tabs defaultValue="curriculum" >
+              <TabsList className="flex justify-center">
+                <TabsTrigger value="curriculum">Contenido</TabsTrigger>
+                <TabsTrigger value="instructor">Docente</TabsTrigger>
+              </TabsList>
+              <TabsContent value="curriculum">
+                <ol className="list-decimal pl-5 space-y-2">
+                  {curso.contenidos.map((item, index) => (
+                    <>
+                      <li key={index}>{item.titulo}</li>
+                      {item.subtitulos && (
+                        <ul className="list-disc pl-5 space-y-2">
+                          {item.subtitulos.map((subitem, subindex) => (
+                            <li key={subindex}>{subitem}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </>
+                  ))}
+                </ol>
+              </TabsContent>
+              <TabsContent value="instructor">
+                <div className="flex items-center space-x-4 mb-4">
+                  <Avatar>
+                    <AvatarImage src={'/images/' + curso.docente.foto} alt={curso.docente.nombre} />
+                  </Avatar>
+                  <div>
+                    <h3 className="font-semibold">{curso.docente.nombre}</h3>
+                    <p className="text-sm text-gray-500">{curso.docente.bio}</p>
+                  </div>
                 </div>
-              </div>
-            </TabsContent>
-          </Tabs>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">
-            Inscribirse
-          </Button>
-        </CardFooter>
-      </Card>
+              </TabsContent>
+            </Tabs>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-40 mx-auto p-6">
+              Inscribirse
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="flex justify-center">
+        <Button variant="ghost" className="mb-4" asChild >
+          <a href="/catalogo">
+            <LibraryBig className="mr-2 h-4 w-4" /> Ver todos los cursos
+          </a>
+        </Button>
+      </div>
+
     </main>
   )
 }
